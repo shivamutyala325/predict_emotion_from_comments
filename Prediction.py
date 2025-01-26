60763b064bbc2c4693a34568f8f16cafb8ad6f44
shivamutyala325 <155558495+shivamutyala325@users.noreply.github.com>
2025-01-26 19:00:02 +0530

    Update and rename predict_cmt_emn.py to Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -3,21 +3,27 @@ import keras
 import numpy as np
 import pandas as pd
 from keras.models import load_model
+
+#load the trained model
 model=load_model(r'text_base.keras')
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import pickle
 
+#importintg the trainer tokenizer
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 
 while True:
+    #take user input
     comment=[input('enter a comment')]
+    #process the comment
     comment_seqence=tokenizer.texts_to_sequences(comment)
     paded_comment=pad_sequences(comment_seqence,maxlen=300,padding='post')
 
+    #send the processed comment to model
     res=model.predict(paded_comment)
     res=np.argmax(res)
     print(res)
